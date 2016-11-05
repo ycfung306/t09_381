aa62a8abe304617ca30521777906026a65ffd009
Developer <comps381f@ouhk.edu.hk>
2016-11-05 16:20:58 +0800

    first commit

--- server.js
+++ server.js
@@ -2,14 +2,20 @@ var MongoClient = require('mongodb').MongoClient;
 var assert = require('assert');
 var ObjectId = require('mongodb').ObjectID;
 // Use your own mlab user id and password!!!
-var mongourl = 'mongodb://student:password@ds031873.mlab.com:31873/comps381f';
+var mongourl = 'mongodb://localhost:27017/test';
 
 var express = require('express');
 var fileUpload = require('express-fileupload');
 var app = express();
 
+var bodyParser = require('body-parser')
+
 // middlewares
 app.use(fileUpload());
+app.use(bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+}));
 
 app.post('/upload', function(req, res) {
     var sampleFile;
@@ -20,9 +26,9 @@ app.post('/upload', function(req, res) {
     }
 
     MongoClient.connect(mongourl,function(err,db) {
-      console.log('Connected to mlab.com');
+      console.log('Connected to db');
       assert.equal(null,err);
-      create(db, req.files.sampleFile, function(result) {
+      create(db, req.files.sampleFile, req.body.caption, new Date(req.body.date), function(result) {
         db.close();
         if (result.insertedId != null) {
           res.status(200);
@@ -33,6 +39,7 @@ app.post('/upload', function(req, res) {
         }
       });
     });
+    
     /*
     sampleFile = req.files.sampleFile;
     sampleFile.mv('/somewhere/on/your/server/filename.jpg', function(err) {
@@ -48,12 +55,12 @@ app.post('/upload', function(req, res) {
 
 app.get('/download', function(req,res) {
   MongoClient.connect(mongourl,function(err,db) {
-    console.log('Connected to mlab.com');
+    console.log('Connected to db');
     console.log('Finding key = ' + req.query.key)
     assert.equal(null,err);
     var bfile;
     var key = req.query.key;
-	  if (key != null) {
+    if (key != null) {
       read(db, key, function(bfile,mimetype) {
         if (bfile != null) {
           console.log('Found: ' + key)
@@ -73,11 +80,24 @@ app.get('/download', function(req,res) {
   });
 });
 
-function create(db,bfile,callback) {
+app.get('/read', function(req, res){
+  MongoClient.connect(mongourl,function(err,db) {
+    console.log('Connected to db');
+    console.log(req.query);
+    search(db, req.query, function(results){
+      res.render('display.ejs', {"photos": results});
+    });
+  });
+});
+
+function create(db,bfile,caption,date,callback) {
   console.log(bfile);
   db.collection('photos').insertOne({
     "data" : new Buffer(bfile.data).toString('base64'),
     "mimetype" : bfile.mimetype,
+    "caption" : caption,
+    "month" : date.getMonth()+1+'',
+    "year" : date.getFullYear()+''
   }, function(err,result) {
     //assert.equal(err,null);
     if (err) {
@@ -100,6 +120,20 @@ function read(db,target,callback) {
       mimetype = doc.mimetype;
     }
     callback(bfile,mimetype);
+  });
+}
+
+function search(db, criteria, callback){
+  var photos = [];
+  cursor = db.collection('photos').find(criteria, {"data": 0});
+  cursor.each(function(err, doc) {
+    assert.equal(err, null); 
+    if (doc != null) {
+      photos.push(doc);
+    } else {
+      console.log(photos);
+      callback(photos);
+    }  
   });
 }
 
